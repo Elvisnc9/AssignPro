f23a04516309265ce40c9e312d03b81f796b57ba
elvisnc <148381430+Elvisnc9@users.noreply.github.com>
2024-11-25 13:06:03 +0100

    CRUD function part2 integrated with MYSQL database

--- app.js
+++ app.js
@@ -1,70 +1,80 @@
 const express = require('express');
 const PORT = 2721;
-
+const mysql = require('mysql2')
 const app = express();
 
 app.use(express.json());
+
+//Here i connected my API to mySql and a database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Ngwudalu12345.',
+  database: 'taskManager'
+})
+connection.connect((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Connected succefully')
+  }
+})
+
 
 
 app.listen(PORT, () => {
   console.log('SERVER IS RUNNING')
 })
 
-var todo = [
-  {
-    id: "1",
-    task: "Code today",
-    completed: true,
-  }
-]
+//I have Already Defined this the Database 
+// var todo = [
+//   {
+//     id: "1",
+//     task: "Code today",
+//     completed: true,
+//   }
+// ]
 
 // GET
-app.get('/GET/todos', (req, res) => {
-  res.send({
-    message: "TO_DO APLICATION",
-    data: todo
-  })
+app.get('/GET/todos/:id', (req, res) => {
+  const fetchid = req.params.id;
+  connection.query('SELECT * FROM tasks WHERE id = ?',fetchid,(err, result) =>{
+if(err){
+  console.log(err)
+}else{
+  res.status(201).send(
+   result
+  )
+} } )
 })
 
-
-//POST and A tiny bit of validations
+//POST 
 app.post('/POST/todos', (req, res) => {
   const { task, completed } = req.body;
-  if (task && typeof completed === 'boolean') {
-    todo.push({
-      id: (todo.length + 1).toString(),
-      task: task,
-      completed: completed
-    })
-    res.status(201).send(
-      {
-        message: "Task Added Successfully",
-      })
-  }
-  else {
-    res.status(400).send(
-      {
-        message: "NULL❌❌❌❌",
-        error: "Task & completed requires a valid input"
-      })
-  }
-});
+  connection.query(
+    'INSERT INTO tasks (task, completed) VALUES (?, ?)',
+    [task, completed],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ error: "Failed to insert task" });
+      } else {
+        res.status(201).send("POSTED SUCESSFULLY");
+      }
+    }
+  );})
 
 
 //DELETE
 app.delete('/DELETE/todos/:id', (req, res) => {
   var id = req.params.id
-  const newtodo = todo.filter(tk => tk.id != id)
-  todo = newtodo;
-
-  res.send(
-    {
-      message: "Task has been deleted successfully",
-      progress: "Done 💯💯"
+  connection.query('DELETE FROM tasks WhERE id=?',id,(err, result)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.status(201).send("DELETED SUCESSFULLY")
     }
-  )
-
-
+  })
 })
 
 
@@ -72,20 +82,15 @@ app.delete('/DELETE/todos/:id', (req, res) => {
 app.put('/PUT/todos/:id', (req, res) => {
   var id = req.params.id;
   const { task, completed } = req.body;
-  var index = todo.findIndex(tk => tk.id == id);
-  if (index === -1) {
-    return res.status(404).send({ message: "Task not found" });
-  }
-  todo[index] = {
-    ...todo[index],
-    task,
-    completed,
-  }
-
-  res.send(
-    {
-      message: "Task has been updated Succefully",
-      progress: "Added ✔️✔️✔️"
+  connection.query('UPDATE tasks SET task =?, completed=? WHERE id=?',[task,completed,id],(err, result)=>{
+    if(err){
+      console.log(err)
+    }else{
+      res.status(201).send("UPDATED SUCESSFULLY")
     }
-  )
+  })
 })
+
+
+
+///DONE✔️✔️
